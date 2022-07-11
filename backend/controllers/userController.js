854887d2e8a4d3be1287534c2c76d2f507439850
kathleen <kathleenmcc@gmail.com>
2022-07-10 23:45:32 -0400

    july

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,7 +61,7 @@ const getMe = asyncHandler(async(req, res) => {
     })
 
 const generateToken = (id)  => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: '30d',
+    return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: '90d',
 })
 }
 
